f7990883655c8207b2e2372918546048bf073000
Bruno Garcia Rana <brunogarciarana@gmail.com>
2021-04-21 13:31:46 -0300

    feat: reactive mongodb configured

---
+++ src/test/java/com/demo/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
